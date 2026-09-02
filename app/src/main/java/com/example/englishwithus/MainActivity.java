397c69c5bdc48b2fa3c9c31284a5264138910b98
Vo Minh Triet <minhtrietvo264@gmail.com>
2026-09-02 13:05:32 +0700

    Initial commit app Englishwithus

---
+++ app/src/main/java/com/example/englishwithus/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.englishwithus;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.myWebView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
